b597d75826a0f8c251388948e386111bb8f67caa
ArtLevel <algitIT@rambler.ru>
2023-07-08 20:03:52 +0300

    Task 10 complete

---
+++ 7-8 kyi/10/10.js
@@ -0,0 +1,29 @@
+/*
+	Trolls are attacking your comment section!
+
+	A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+	Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+	For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+	Note: for this kata y isn't considered a vowel.
+
+ */
+
+function disemvowel(str) {
+	const vowels = {
+		'a': 'a',
+		'e': 'e',
+		'i': 'i',
+		'o': 'o',
+		'u': 'u',
+	}
+
+	return str.split('').map(symbol => {
+		if(!vowels[symbol.toLowerCase()]) {
+			return symbol
+		} else {}
+		return ''
+	}).join('')
+}
