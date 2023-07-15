eac0cda383853f26f20c322d0b2a8d9732b125e0
ArtLevel <algitIT@rambler.ru>
2023-07-15 13:59:06 +0300

    Task 24 complete

---
+++ 7-8 kyi/24/24.js
@@ -0,0 +1,16 @@
+function sumOfMinimums(arr) {
+	const minNumOfArrays = []
+
+	for(let i = 0; i < arr.length; i++) {
+		const nestedArr =	arr[i]
+		const minNum = Math.min.apply(null, nestedArr)
+
+		minNumOfArrays.push(minNum)
+	}
+
+	const sumOfNum = minNumOfArrays.reduce((num, curValue) => num + curValue, 0)
+
+	return sumOfNum
+}
+
+console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]))
