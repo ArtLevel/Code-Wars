afc9e7e2491dcd1a7d32adccd09d63ae261b210a
ArtLevel <algitIT@rambler.ru>
2023-07-29 20:57:50 +0300

    Task 15 complete

---
+++ 6 kyi/15/15.js
@@ -0,0 +1,11 @@
+function toCamelCase(str) {
+	const symbolsArr = str.split('')
+
+	symbolsArr.forEach((el, index) => {
+		if(el === '-' || el === '_') symbolsArr[index + 1] = symbolsArr[index + 1].toUpperCase()
+	})
+
+	return symbolsArr.filter(el => el !== '-' && el !== '_').join('')
+}
+
+console.log(toCamelCase('the_stealth_warrior'))
