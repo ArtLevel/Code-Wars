3ae407c2dafb7b6556b9df61aa2f6fc58f1e4352
ArtLevel <algitIT@rambler.ru>
2023-07-30 18:42:15 +0300

    Fix bug in 34 task

--- 7-8 kyi/34/34.js
+++ 7-8 kyi/34/34.js
@@ -2,7 +2,7 @@ function powerOf4(n) {
 	if (!Number.isInteger(n)) return false
 	const sqrt = Math.log(n) / Math.log(4)
 
-	return sqrt % 2 === 0
+	return Number.isInteger(sqrt)
 }
 
-console.log(powerOf4(16384))
+console.log(powerOf4(64))
