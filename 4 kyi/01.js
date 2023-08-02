5d80dd67b8746de4a3606bc4d14515562a6a98ea
ArtLevel <algitIT@rambler.ru>
2023-08-02 20:18:33 +0300

    Task 1 refactro

--- 4 kyi/01.js
+++ 4 kyi/01.js
@@ -1,5 +1,3 @@
-function sumStrings(a, b) {
-	return (BigInt(a) + BigInt(b)).toString()
-}
+const sumStrings = (a, b) => (BigInt(a) + BigInt(b)).toString()
 
 console.log(sumStrings('1000000000000000000000000000000000000000000000000000000', '222'))
