9c23bb87f312345add8c28af0076dc8feb799327
ArtLevel <algitIT@rambler.ru>
2023-08-02 20:11:22 +0300

    Task 1 complete

--- 4 kyi/01.js
+++ 4 kyi/01.js
@@ -1,26 +1,5 @@
 function sumStrings(a, b) {
-	const arrA = a.split('').map(Number)
-	const arrB = b.split('').map(Number)
-
-	const result = []
-	const count = arrA.length > arrB.length ? arrA.length : arrB.length
-
-	for (let i = 0; i < count; i++) {
-		if (arrA[i] && arrB[i]) {
-			result.push(arrA[i] + arrB[i])
-		}
-
-		if (!arrA[i]) {
-			result.push(arrB[i])
-		}
-
-		if (!arrB[i]) {
-			result.push(arrA[i])
-		}
-
-	}
-
-	return result.join('')
+	return (BigInt(a) + BigInt(b)).toString()
 }
 
-console.log(sumStrings('8797', '45'))
+console.log(sumStrings('1000000000000000000000000000000000000000000000000000000', '222'))
