40d02849e9aeca15b6f493275f918d72887af737
ArtLevel <algitIT@rambler.ru>
2023-08-25 11:50:35 +0300

    42 complete

--- 7-8 kyi/42/01.js
+++ 7-8 kyi/42/01.js
@@ -1,1 +1,6 @@
-const printArray = (a) => a.join()
+const solution = (a, b) => {
+	if(a.length > b.length) return b + a + b
+	if(a.length < b.length) return a + b + a
+}
+
+console.log(solution('13', '200'))
