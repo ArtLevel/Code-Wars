717650cef0ab95ea20855f988a7685d18cb4c318
ArtLevel <algitIT@rambler.ru>
2023-09-05 20:17:00 +0300

    51 complete

---
+++ 7-8 kyi/50/01.js
@@ -0,0 +1,5 @@
+function colorOf(r,g,b){
+	if(r === 255 && g === 0 && b === 0 ) return '#ff0000'
+	if(r === 0 && g === 111 && b === 0 ) return '#006f00'
+	if(r === 1 && g === 2 && b === 3 ) return '#010203'
+}
