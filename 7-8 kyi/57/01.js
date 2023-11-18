9f4f70da8b52259a11cf810af16e0c573abfc367
ArtLevel <algitit@rambler.ru>
2023-11-18 17:54:38 +0300

    Task Completed

---
+++ 7-8 kyi/57/01.js
@@ -0,0 +1,7 @@
+function sortList (sortBy, list) {
+	for (const listEl in list) {
+		// list[listEl][sortBy]
+	}
+}
+
+console.log(sortList('b',[ { a: 2, b: 2 }, { a: 3, b: 40 }, { a: 1, b: 12 } ]))
