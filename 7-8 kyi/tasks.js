a5cd1d2d5baae07d3239fe7712463a7c8f506ef6
ArtLevel <algitit@rambler.ru>
2023-12-21 12:17:12 +0300

    Task Completed: Find the missing letter

--- 7-8 kyi/tasks.js
+++ 7-8 kyi/tasks.js
@@ -1,49 +1,52 @@
-const getLengthOfMissingArray = (arrayOfArrays) =>{
-	if (!arrayOfArrays) return 0
-	if (arrayOfArrays.length === 0) return 0
+// 6 kyu
+// Length of missing array
 
-	if(arrayOfArrays.some(el => !el)) {
-		return 0
-	}
-
-	let result = 0
-
-	arrayOfArrays.forEach(el => {
-		if(el.length === 0) result = false
-	})
-
-	if(result === false) {
-		return 0
-	}
-
-	const sortedArr = [...arrayOfArrays].sort((arr, nextArr) => arr.length - nextArr.length)
-	let length = 0
-	let lengthOfTheFirstItemOfArr = 0
-
-	sortedArr.forEach((el, i) => {
-		if(i === 0) {
-			length = el.length
-			lengthOfTheFirstItemOfArr = el.length
-		}
-
-		if(el.length > length && el.length - length === 1) {
-			length = el.length
-		} else {
-			length = el.length
-			result = i
-		}
-	})
-
-	return result + lengthOfTheFirstItemOfArr
-}
-
-console.log(getLengthOfMissingArray(
-	[],
-	[4, 1, 3, 1],
-	[3],
-	[0, 2],
-	[0, 0, 3, 2, 4]
-)) // 0
+// const getLengthOfMissingArray = (arrayOfArrays) =>{
+// 	if (!arrayOfArrays) return 0
+// 	if (arrayOfArrays.length === 0) return 0
+//
+// 	if(arrayOfArrays.some(el => !el)) {
+// 		return 0
+// 	}
+//
+// 	let result = 0
+//
+// 	arrayOfArrays.forEach(el => {
+// 		if(el.length === 0) result = false
+// 	})
+//
+// 	if(result === false) {
+// 		return 0
+// 	}
+//
+// 	const sortedArr = [...arrayOfArrays].sort((arr, nextArr) => arr.length - nextArr.length)
+// 	let length = 0
+// 	let lengthOfTheFirstItemOfArr = 0
+//
+// 	sortedArr.forEach((el, i) => {
+// 		if(i === 0) {
+// 			length = el.length
+// 			lengthOfTheFirstItemOfArr = el.length
+// 		}
+//
+// 		if(el.length > length && el.length - length === 1) {
+// 			length = el.length
+// 		} else {
+// 			length = el.length
+// 			result = i
+// 		}
+// 	})
+//
+// 	return result + lengthOfTheFirstItemOfArr
+// }
+//
+// console.log(getLengthOfMissingArray(
+// 	[],
+// 	[4, 1, 3, 1],
+// 	[3],
+// 	[0, 2],
+// 	[0, 0, 3, 2, 4]
+// )) // 0
 //
 // console.log(getLengthOfMissingArray(
 // 	[]
@@ -57,10 +60,43 @@ console.log(getLengthOfMissingArray(
 // console.log(getLengthOfMissingArray(
 // 	[ [], [ 1, 2, 2 ] ]
 // ))
+//
+//
+// console.log(getLengthOfMissingArray(
+// 	[ [ 1, 2, 2 ], null ]
+// ))
+//
+//
 
+// 6 kyu
+// Find the missing letter
 
-console.log(getLengthOfMissingArray(
-	[ [ 1, 2, 2 ], null ]
-))
+const findMissingLetter = (array) => {
+	const letters = [
+		'a', 'b', 'c', 'd', 'e',
+		'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z'
+	]
 
+	let idTheFirstLetter
+	let count = 1
+	let result = ''
 
+	array.map(el => el.toLowerCase()).forEach((el, i) => {
+		if(i === 0) idTheFirstLetter = letters.indexOf(el)
+
+		if(i > 0) {
+			if(el === letters[idTheFirstLetter + count]) {
+				count += 1
+			} else {
+				result = letters[idTheFirstLetter + count]
+			}
+		}
+	})
+
+	return array[0].toUpperCase() === array[0] ? result.toUpperCase() : result
+}
+
+console.log(findMissingLetter(['c','d','e','f','h','i']))
